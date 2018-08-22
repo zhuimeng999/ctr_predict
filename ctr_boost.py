256bef25eb1b3fa8800122ab8e114367b53a75bd
lucius <897976524@qq.com>
2018-08-23 02:09:07 +0800

    boost

--- ctr_boost.py
+++ ctr_boost.py
@@ -14,11 +14,11 @@
 # ==============================================================================
 """Train DNN on Kaggle movie dataset."""
 
-import os
+import os, re
 
 import tensorflow as tf
 from ctr_dataset import build_model_columns, get_input_fn, FLAGS
-tf.GraphKeys
+
 
 tf.app.flags.DEFINE_integer(
     "epochs_to_train", 15,
@@ -26,8 +26,8 @@ tf.app.flags.DEFINE_integer(
     "completely.")
 
 
-def model_tanh(net, units, trainable):
-    with tf.variable_scope(None, default_name='tanh'):
+def model_tanh(net, scope, units, trainable):
+    with tf.variable_scope(scope, default_name='tanh'):
         net1 = tf.layers.dense(net, units=units, activation=tf.nn.tanh, trainable=trainable)
         net2 = tf.layers.dense(net1, units=units, activation=tf.nn.tanh, trainable=trainable)
         logists = tf.layers.dense(net2, units=1, activation=None, trainable=trainable)
@@ -35,8 +35,8 @@ def model_tanh(net, units, trainable):
     return net1, net2, logists
 
 
-def model_selu(net, units, trainable):
-    with tf.variable_scope(None, default_name='selu'):
+def model_selu(net, scope, units, trainable):
+    with tf.variable_scope(scope, default_name='selu'):
         net1 = tf.layers.dense(net, units=units, activation=tf.nn.selu, trainable=trainable)
         net2 = tf.layers.dense(net1, units=units, activation=tf.nn.selu, trainable=trainable)
         logists = tf.layers.dense(net2, units=1, activation=None, trainable=trainable)
@@ -44,8 +44,8 @@ def model_selu(net, units, trainable):
     return net1, net2, logists
 
 
-def model_relu(net, units, trainable):
-    with tf.variable_scope(None, default_name='relu'):
+def model_relu(net, scope, units, trainable):
+    with tf.variable_scope(scope, default_name='relu'):
         net1 = tf.layers.dense(net, units=units, activation=tf.nn.relu, trainable=trainable)
         net2 = tf.layers.dense(net1, units=units, activation=tf.nn.relu, trainable=trainable)
         logists = tf.layers.dense(net2, units=1, activation=None, trainable=trainable)
@@ -56,7 +56,7 @@ def model_relu(net, units, trainable):
 model_factories = [model_tanh, model_selu, model_relu]
 
 
-def my_model(features, labels, mode, params):
+def my_model(features=None, labels=None, mode=None, config=None, params=None):
     """DNN with three hidden layers, and dropout of 0.1 probability."""
     # Create three fully connected layers each layer having a dropout
     # probability of 0.1.
@@ -67,10 +67,12 @@ def my_model(features, labels, mode, params):
     assert 'models' in params and isinstance(params['models'], (list, tuple)) and len(params['models']) > 0
 
     models = []
-    if len(params['models']) > 1:
-        for model in params['models'][:-1]:
-            models.append(model_factories[model](net, input_dim[1], False))
-    models.append(model_factories[params['models'][-1]](net, input_dim[1], True))
+    epoch_value = params['epoch_value']
+    process_state = epoch_value // 5
+    for i in range(process_state):
+        models.append(params['models'][i](net, 'model' + str(i), input_dim[1], False))
+    new_model_scope = 'model' + str(len(models))
+    models.append(params['models'][process_state](net, new_model_scope, input_dim[1], True))
     #print(models[0][1].kernel)
     logists = sum(map(lambda x: x[-1], models))
     # Compute predictions.
@@ -102,41 +104,106 @@ def my_model(features, labels, mode, params):
     optimizer = tf.train.AdadeltaOptimizer(learning_rate=0.001)
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
-    var_list = list(filter(lambda x: x not in models[-1], tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)))
-    print(list(map(lambda x: x.name, var_list)))
-    scaffold = tf.train.Scaffold(saver=tf.train.Saver(var_list=var_list))
-    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, scaffold=scaffold)
+    basic_list = []
+    extand_list = []
+    for tensor_var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+        if re.match('.*' + new_model_scope + '/.*', tensor_var.name):
+            extand_list.append(tensor_var)
+        else:
+            basic_list.append(tensor_var)
+
+    class MySaver(tf.train.Saver):
+        def __init__(self, basic_list=None, extand_list=None):
+            if basic_list is None:
+                self._basic_saver = None
+                full_list = extand_list
+            else:
+                self._basic_saver = tf.train.Saver(var_list=basic_list)
+                full_list = [*basic_list, *extand_list]
+            super().__init__(var_list=full_list)
+            self._basic_list = basic_list
+            self._extand_list = extand_list
+            self._is_restored = False
+
+        def restore(self, sess, save_path):
+            try:
+                super().restore(sess, save_path)
+            except tf.errors.NotFoundError as e:
+                assert not self._is_restored
+
+                sess.run([v.initializer for v in self._extand_list])
+                if self._basic_saver:
+                    self._basic_saver.restore(sess, save_path)
+            self._is_restored = True
+
+    scaffold = tf.train.Scaffold(saver=MySaver(basic_list=basic_list, extand_list=extand_list))
+    hook = tf.train.CheckpointSaverHook(
+        config.model_dir,
+        save_secs=config.save_checkpoints_secs,
+        save_steps=config.save_checkpoints_steps,
+        scaffold=scaffold)
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, scaffold=scaffold, training_hooks=[hook])
+
+
+class boost(tf.estimator.Estimator):
+    def __init__(self, model_fn, model_dir=None, config=None, params=None,
+                 warm_start_from=None):
+        super().__init__(model_fn=model_fn, model_dir=model_dir, config=config, params=params, warm_start_from=warm_start_from)
+
+        def _train(input_fn, hooks=None, steps=None, max_steps=None):
+            ckpt = tf.train.get_checkpoint_state(self._model_dir)
+            if ckpt:
+                print(ckpt.model_checkpoint_path)
+                reader = tf.train.NewCheckpointReader(ckpt.model_checkpoint_path)
+                self._params['epoch_value'] = reader.get_tensor('epoch_tensor')
+                print('############epoch_value', self._params['epoch_value'])
+            else:
+                self._params['epoch_value'] = 0
+            top_self = self
+
+            class MyHook(tf.train.SessionRunHook):
+                def end(self, session):
+                    print(session.run([top_self._epoch_add_tensor]))
+                    return super().end(session)
+
+            saver_hook = MyHook()
+            if hooks:
+                hooks.append(saver_hook)
+            else:
+                hooks = [saver_hook]
+            result = super(boost, self).train(input_fn=input_fn, hooks=hooks, steps=steps, max_steps=max_steps)
+
+            return result
+        self.train = _train
+
+    def _call_model_fn(self, features, labels, mode, config):
+        epoch_tensor = tf.get_variable('epoch_tensor', shape=(), dtype=tf.int32, initializer=tf.zeros_initializer,trainable=False)
+        epoch_add_tensor = tf.assign_add(epoch_tensor, 1)
+        self._epoch_tensor = epoch_tensor
+        self._epoch_add_tensor = epoch_add_tensor
+        return super()._call_model_fn(features, labels, mode, config)
 
 
 def main(_):
     # Build 2 hidden layer DNN with 10, 10 units respectively.
     if True:
         ws = tf.estimator.WarmStartSettings(
-            ckpt_to_initialize_from="/home/lucius/Projects/notebook/homework/ctr_predict/models/v1",
-            vars_to_warm_start="(.*input_layer.*|tanh/dense(|_1|_2)/.*)",
-            var_name_to_prev_var_name={
-                'tanh/dense/kernel:0':'dense/kernel',
-                'tanh/dense/bias:0':'dense/bias',
-                'tanh/dense_1/kernel:0': 'dense_1/kernel',
-                'tanh/dense_1/bias:0': 'dense_1/bias',
-                'tanh/dense_2/kernel:0': 'dense_2/kernel',
-                'tanh/dense_2/bias:0': 'dense_2/bias',
-            })
+            ckpt_to_initialize_from="/home/lucius/Projects/notebook/homework/ctr_predict/models/v2",
+            vars_to_warm_start=".*input_layer/.*")
     else:
         ws = None
-
-    classifier = tf.estimator.Estimator(
+    classifier = boost(
         model_fn=my_model,
         params={
             'feature_columns': build_model_columns(),
-            'models': [0, 1]
+            'models': model_factories
         },
         model_dir=FLAGS.output_dir,
         warm_start_from=ws)
-
     for i in range(FLAGS.epochs_to_train):
         print('perform {}s epoch...'.format(i))
         # Train the Model.
+
         classifier.train(
             input_fn=get_input_fn(os.path.join('prev-output', 'train_format.tfrecord'), 256, 1, 1000, use_tfrecord=True))
 
